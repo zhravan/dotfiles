ae6ba431d6b55e694d6606e7bcf595c4da8d802f
S K <47430686+shravan20@users.noreply.github.com>
2023-09-12 18:48:39 +0530

    Update zsh-setup.sh

--- workstation/zsh-setup.sh
+++ workstation/zsh-setup.sh
@@ -12,5 +12,7 @@ source ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
 source ~/.zshrc
 
