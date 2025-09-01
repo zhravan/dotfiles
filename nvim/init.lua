4263b50a4affb2dbbfc8577703c13a186b4a283a
zhravan <mrshravankumarb@gmail.com>
2025-09-01 22:20:03 +0000

    nvim: bootstrap lazy.nvim and load config modules

---
+++ nvim/init.lua
@@ -0,0 +1,36 @@
+-- Set leader keys early
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Core settings
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+-- Load plugins
+require("lazy").setup({
+  { import = "plugins" },
+}, {
+  ui = {
+    border = "rounded",
+  },
+  change_detection = {
+    notify = false,
+  },
+})
+
+
